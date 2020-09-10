e3d6ae284e54457a70e65cd0fe092e0c415780b8
xm <402276694@qq.com>
2020-09-10 20:51:11 +0800

    集成minitouch 调整页面布局

--- app.js
+++ app.js
@@ -3,32 +3,34 @@ var WebSocketServer = require('ws').Server
   , express = require('express')
   , path = require('path')
   , net = require('net')
-  , app = express()
+  , app = express();
 
-var PORT = process.env.PORT || 9002
+var PORT = process.env.PORT || 9002;
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')));
 
-var server = http.createServer(app)
-var wss = new WebSocketServer({ server: server })
+var server = http.createServer(app);
 
-wss.on('connection', function(ws) {
-  console.info('Got a client')
+const webSocketServerForMinicap = new WebSocketServer({ noServer: true });
+const webSocketServerForMinitouch = new WebSocketServer({ noServer: true });
 
-  var stream = net.connect({
+webSocketServerForMinicap.on('connection', function(ws) {
+  console.info('WebSocketServerForMinicap Got a client');
+
+  var minicapStream = net.connect({
     port: 1717
-  })
+  });
 
-  stream.on('error', function() {
-    console.error('Be sure to run `adb forward tcp:1717 localabstract:minicap`')
-    process.exit(1)
-  })
+  minicapStream.on('error', function() {
+    console.error('Be sure to run `adb forward tcp:1717 localabstract:minicap`');
+    process.exit(1);
+  });
 
-  var readBannerBytes = 0
-  var bannerLength = 2
-  var readFrameBytes = 0
-  var frameBodyLength = 0
-  var frameBody = new Buffer(0)
+  var readBannerBytes = 0;
+  var bannerLength = 2;
+  var readFrameBytes = 0;
+  var frameBodyLength = 0;
+  var frameBody = new Buffer(0);
   var banner = {
     version: 0
   , length: 0
@@ -39,133 +41,173 @@ wss.on('connection', function(ws) {
   , virtualHeight: 0
   , orientation: 0
   , quirks: 0
-  }
-
+  };
   function tryRead() {
-    for (var chunk; (chunk = stream.read());) {
-      console.info('chunk(length=%d)', chunk.length)
+    for (var chunk; (chunk = minicapStream.read());) {
+      console.info('chunk(length=%d)', chunk.length);
       for (var cursor = 0, len = chunk.length; cursor < len;) {
         if (readBannerBytes < bannerLength) {
           switch (readBannerBytes) {
           case 0:
             // version
-            banner.version = chunk[cursor]
-            break
+            banner.version = chunk[cursor];
+            break;
           case 1:
             // length
-            banner.length = bannerLength = chunk[cursor]
-            break
+            banner.length = bannerLength = chunk[cursor];
+            break;
           case 2:
           case 3:
           case 4:
           case 5:
             // pid
             banner.pid +=
-              (chunk[cursor] << ((readBannerBytes - 2) * 8)) >>> 0
-            break
+              (chunk[cursor] << ((readBannerBytes - 2) * 8)) >>> 0;
+            break;
           case 6:
           case 7:
           case 8:
           case 9:
             // real width
             banner.realWidth +=
-              (chunk[cursor] << ((readBannerBytes - 6) * 8)) >>> 0
-            break
+              (chunk[cursor] << ((readBannerBytes - 6) * 8)) >>> 0;
+            break;
           case 10:
           case 11:
           case 12:
           case 13:
             // real height
             banner.realHeight +=
-              (chunk[cursor] << ((readBannerBytes - 10) * 8)) >>> 0
-            break
+              (chunk[cursor] << ((readBannerBytes - 10) * 8)) >>> 0;
+            break;
           case 14:
           case 15:
           case 16:
           case 17:
             // virtual width
             banner.virtualWidth +=
-              (chunk[cursor] << ((readBannerBytes - 14) * 8)) >>> 0
-            break
+              (chunk[cursor] << ((readBannerBytes - 14) * 8)) >>> 0;
+            break;
           case 18:
           case 19:
           case 20:
           case 21:
             // virtual height
             banner.virtualHeight +=
-              (chunk[cursor] << ((readBannerBytes - 18) * 8)) >>> 0
-            break
+              (chunk[cursor] << ((readBannerBytes - 18) * 8)) >>> 0;
+            break;
           case 22:
             // orientation
-            banner.orientation += chunk[cursor] * 90
-            break
+            banner.orientation += chunk[cursor] * 90;
+            break;
           case 23:
             // quirks
-            banner.quirks = chunk[cursor]
-            break
+            banner.quirks = chunk[cursor];
+            break;
           }
 
-          cursor += 1
-          readBannerBytes += 1
+          cursor += 1;
+          readBannerBytes += 1;
 
           if (readBannerBytes === bannerLength) {
-            console.log('banner', banner)
+            console.log('banner', banner);
           }
         }
         else if (readFrameBytes < 4) {
-          frameBodyLength += (chunk[cursor] << (readFrameBytes * 8)) >>> 0
-          cursor += 1
-          readFrameBytes += 1
-          console.info('headerbyte%d(val=%d)', readFrameBytes, frameBodyLength)
+          frameBodyLength += (chunk[cursor] << (readFrameBytes * 8)) >>> 0;
+          cursor += 1;
+          readFrameBytes += 1;
+          console.info('headerbyte%d(val=%d)', readFrameBytes, frameBodyLength);
         }
         else {
           if (len - cursor >= frameBodyLength) {
-            console.info('bodyfin(len=%d,cursor=%d)', frameBodyLength, cursor)
+            console.info('bodyfin(len=%d,cursor=%d)', frameBodyLength, cursor);
 
             frameBody = Buffer.concat([
               frameBody
             , chunk.slice(cursor, cursor + frameBodyLength)
-            ])
+            ]);
 
             // Sanity check for JPG header, only here for debugging purposes.
             if (frameBody[0] !== 0xFF || frameBody[1] !== 0xD8) {
               console.error(
-                'Frame body does not start with JPG header', frameBody)
-              process.exit(1)
+                'Frame body does not start with JPG header', frameBody);
+              process.exit(1);
             }
 
             ws.send(frameBody, {
               binary: true
-            })
+            });
 
-            cursor += frameBodyLength
-            frameBodyLength = readFrameBytes = 0
-            frameBody = new Buffer(0)
+            cursor += frameBodyLength;
+            frameBodyLength = readFrameBytes = 0;
+            frameBody = new Buffer(0);
           }
           else {
-            console.info('body(len=%d)', len - cursor)
+            console.info('body(len=%d)', len - cursor);
 
             frameBody = Buffer.concat([
               frameBody
             , chunk.slice(cursor, len)
-            ])
+            ]);
 
-            frameBodyLength -= len - cursor
-            readFrameBytes += len - cursor
-            cursor = len
+            frameBodyLength -= len - cursor;
+            readFrameBytes += len - cursor;
+            cursor = len;
           }
         }
       }
     }
   }
 
-  stream.on('readable', tryRead)
+  minicapStream.on('readable', tryRead);
 
   ws.on('close', function() {
-    console.info('Lost a client')
-    stream.end()
+    console.info('Lost a client');
+    minicapStream.end();
   })
-})
+});
 
-server.listen(PORT)
-console.info('Listening on port %d', PORT)
+webSocketServerForMinitouch.on('connection', function connection(ws) {
+    console.info('WebSocketServerForMinitouch Got a client');
+    // ...
+
+    var minitouchStream = net.connect({
+        port: 1111
+    });
+    minitouchStream.setNoDelay(true);
+
+    minitouchStream.on('error', function() {
+        console.error('Be sure to run `adb forward tcp:1111 localabstract:minitouch`');
+        process.exit(1);
+    });
+
+    ws.on('message', function(data) {
+        console.info('receive data:'+data);
+        minitouchStream.write(data);
+    });
+
+    ws.on('close', function() {
+        console.info('Lost a client');
+        minitouchStream.end();
+    })
+});
+
+server.on('upgrade', function upgrade(request, socket, head) {
+    const pathname = url.parse(request.url).pathname;
+
+    if (pathname === '/minicap') {
+        webSocketServerForMinicap.handleUpgrade(request, socket, head, function done(ws) {
+            webSocketServerForMinicap.emit('connection', ws, request);
+        });
+    } else if (pathname === '/minitouch') {
+        webSocketServerForMinitouch.handleUpgrade(request, socket, head, function done(ws) {
+            webSocketServerForMinitouch.emit('connection', ws, request);
+        });
+    } else {
+        socket.destroy();
+    }
+});
+
+server.listen(PORT);
+console.info('Listening on port %d', PORT);
--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #/bin/sh
 
-adb push ./minicap/bin/armeabi-v7a/minicap /data/local/tmp/
-adb push ./minicap/shared/android-19/armeabi-v7a/minicap.so  /data/local/tmp/
+adb push ./node_modules/minicap-prebuilt/prebuilt/armeabi-v7a/bin/minicap /data/local/tmp/
+adb push ./node_modules/minicap-prebuilt/prebuilt/armeabi-v7a/lib/android-19/minicap.so  /data/local/tmp/
 adb shell "chmod 777 /data/local/tmp/minicap /data/local/tmp/minicap.so"
+
+adb push ./node_modules/minitouch-prebuilt/prebuilt/armeabi-v7a/bin/minitouch /data/local/tmp/
+adb shell "chmod 777 /data/local/tmp/minitouch"
 
 adb shell LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/minicap -P 1080x1920@540x960/0
 adb forward tcp:1717 localabstract:minicap
 
+adb shell /data/local/tmp/minitouch
+adb forward tcp:1111 localabstract:minitouch
