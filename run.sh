3f8cb23308e615631f08b713aa98046f913eedd5
xm <402276694@qq.com>
2020-09-27 19:27:29 +0800

    自动运行脚本

--- run.sh
+++ run.sh
@@ -1,14 +1,23 @@
-#/bin/sh
+#!/bin/sh
+cpu_arch=`adb shell getprop ro.product.cpu.abi`
+api_ver_num=`adb shell getprop ro.build.version.sdk`
+adb push ./node_modules/minicap-prebuilt/prebuilt/$cpu_arch/bin/minicap /data/local/tmp/
+adb push ./node_modules/minicap-prebuilt/prebuilt/$cpu_arch/lib/android-$api_ver_num/minicap.so  /data/local/tmp/
+adb push ./node_modules/minitouch-prebuilt/prebuilt/$cpu_arch/bin/minitouch /data/local/tmp/
 
-adb push ./node_modules/minicap-prebuilt/prebuilt/armeabi-v7a/bin/minicap /data/local/tmp/
-adb push ./node_modules/minicap-prebuilt/prebuilt/armeabi-v7a/lib/android-19/minicap.so  /data/local/tmp/
-adb shell "chmod 777 /data/local/tmp/minicap /data/local/tmp/minicap.so"
+adb shell ps | grep minicap >/dev/null 2>&1
+[ $? -eq 1 ] && nohup adb shell LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/minicap -P 1080x1920@540x960/0 -Q 30 -S >/dev/null 2>&1 &
 
-adb push ./node_modules/minitouch-prebuilt/prebuilt/armeabi-v7a/bin/minitouch /data/local/tmp/
-adb shell "chmod 777 /data/local/tmp/minitouch"
+adb shell ps | grep minitouch >/dev/null 2>&1
+[ $? -eq 1 ] && nohup adb shell /data/local/tmp/minitouch >/dev/null 2>&1 &
 
-adb shell LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/minicap -P 1080x1920@540x960/0
-adb forward tcp:1717 localabstract:minicap
+netstat -apn | grep 1717 | grep adb >/dev/null 2>&1
+[ $? -eq 1 ] && adb forward tcp:1717 localabstract:minicap
 
-adb shell /data/local/tmp/minitouch
-adb forward tcp:1111 localabstract:minitouch
+netstat -apn | grep 1111 | grep adb >/dev/null 2>&1
+[ $? -eq 1 ] && adb forward tcp:1111 localabstract:minitouch
+
+ps -ef | grep node | grep app >/dev/null 2>&1
+[ $? -eq 1 ] && nohup  node app.js  >/dev/null 2>&1 &
+
+# adb shell svc power stayon true && adb shell input keyevent 26
